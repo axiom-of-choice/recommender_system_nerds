9628da428e7d5c33af7c7b91b3e0883ebee68d8b
Isaac Hernandez <alefsubcero@ciencias.unam.mx>
2022-01-12 20:26:34 -0600

    Not working

--- app.py
+++ app.py
@@ -4,35 +4,34 @@ from urllib.request import urlopen
 
 from flask import Flask, request, jsonify, render_template
 import numpy as np
+import
 
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import load_model
+import pymysql
 
 model = load_model("./model/model.h5")
 
 app = Flask(__name__)
 
+#Enter here your database informations
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "testingdb"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
-def predecir_im(im, invertir=True):
-    image = img_to_array(im)
-    image.shape
 
-    # Scale the image pixels by 255 (or use a scaler from sklearn here)
-    image /= 255
-
-    # Flatten into a 1x28*28 array
-    img = image.flatten().reshape(-1, 28 * 28)
-    img.shape
-    if invertir:
-        img = 1 - img
-
-    # plt.imshow(img.reshape(28, 28), cmap=plt.cm.Greys)
-
-    resultado = model.predict(img)
-    resultado = np.argmax(resultado, axis=-1)
-    return int(resultado[0])
-
+@app.route("/livesearch",methods=["POST","GET"])
+def livesearch():
+    searchbox = request.form.get("text")
+    connection = pymysql.connect()
+    cursor = mysql.connection.cursor()
+    query = "select * from books where title LIKE '{}%' order by title".format(searchbox)
+    cursor.execute(query)
+    result = cursor.fetchall()
+    return jsonify(result)
 
 @app.route("/")
 def index():
@@ -67,3 +66,23 @@ def main():
 
 if __name__ == "__main__":
     app.run()
+
+
+'''def predecir_im(im, invertir=True):
+    image = img_to_array(im)
+    image.shape
+
+    # Scale the image pixels by 255 (or use a scaler from sklearn here)
+    image /= 255
+
+    # Flatten into a 1x28*28 array
+    img = image.flatten().reshape(-1, 28 * 28)
+    img.shape
+    if invertir:
+        img = 1 - img
+
+    # plt.imshow(img.reshape(28, 28), cmap=plt.cm.Greys)
+
+    resultado = model.predict(img)
+    resultado = np.argmax(resultado, axis=-1)
+    return int(resultado[0])'''
